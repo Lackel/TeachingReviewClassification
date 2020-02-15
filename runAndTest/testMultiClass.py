33aff210ae7cbaa1dfc3c660b1710e30c365d972
Lackel <39994881+Lackel@users.noreply.github.com>
2020-02-15 12:42:19 +0800

    第一次提交程序

---
+++ runAndTest/testMultiClass.py
@@ -0,0 +1,52 @@
+#   _*_coding:utf-8 _*_
+#   author:wenbinan
+#   time:2020/2/14 19:09
+#   filename:testMultiLabel.py
+#   product:PyCharm
+
+import time
+import torch
+import numpy as np
+from utils.utils_multiClass.train_eval import train, init_network
+from importlib import import_module
+import argparse
+from tensorboardX import SummaryWriter
+from torch.autograd import Variable
+import os
+
+
+os.chdir("..")   # 修改当前工作目录
+model_name = "TextCNN"
+dataset = 'data/multiClass'
+path_state_dict = "./data/multiClass/saved_dict/TextCNN.ckpt"
+embedding = 'embedding_SougouNews.npz'
+state_dict_load = torch.load(path_state_dict)
+
+if model_name == 'FastText':
+    from utils.utils_multiClass.utils_fasttext import build_dataset, build_iterator, get_time_dif
+
+    embedding = 'random'
+else:
+    from utils.utils_multiClass.utils import build_dataset, build_iterator, get_time_dif
+
+x = import_module('models.multiClass.' + model_name)
+config = x.Config(dataset, embedding)
+np.random.seed(1)
+torch.manual_seed(1)
+torch.cuda.manual_seed_all(1)
+torch.backends.cudnn.deterministic = True  # 保证每次结果一样
+
+vocab, train_data, dev_data, test_data = build_dataset(config, False)
+config.n_vocab = len(vocab)
+test_iter = build_iterator(test_data, config)
+model = x.Model(config).to(config.device)
+
+model.load_state_dict(state_dict_load)
+
+# MultiClass
+for texts, labels in test_iter:
+    outputs = model(texts)
+    predic = torch.max(outputs.data, 1)[1].cpu()
+    print(predic)
+
+
